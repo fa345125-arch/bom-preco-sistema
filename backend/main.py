aa73db8fe55861176dd7f7984305caf2d5b5f518
fa345125-arch <fa345125@gmail.com>
2026-03-12 19:29:43 -0300

    Atualizar o main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from backend.routers import funcionarios
 
 app = FastAPI(title="Sistema Bom Preço")
+
+app.include_router(funcionarios.router)
 
 @app.get("/")
 def home():
