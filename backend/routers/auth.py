21285a3b700c5da0ee0cdc359ab1508c1912ff90
fa345125-arch <fa345125@gmail.com>
2026-03-12 20:05:05 -0300

    Criar auth.py

---
+++ backend/routers/auth.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from database.database import SessionLocal
+from models.usuario import Usuario
+from schemas.usuario import UsuarioCreate, UsuarioLogin
+
+router = APIRouter(prefix="/auth", tags=["Auth"])
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.post("/register")
+def register(user: UsuarioCreate, db: Session = Depends(get_db)):
+    novo = Usuario(**user.dict())
+    db.add(novo)
+    db.commit()
+    return {"status": "usuario criado"}
+
+
+@router.post("/login")
+def login(user: UsuarioLogin, db: Session = Depends(get_db)):
+
+    usuario = db.query(Usuario).filter(
+        Usuario.username == user.username
+    ).first()
+
+    if not usuario:
+        return {"erro": "usuario não encontrado"}
+
+    if usuario.senha != user.senha:
+        return {"erro": "senha incorreta"}
+
+    return {"status": "login realizado", "usuario": usuario.username}
