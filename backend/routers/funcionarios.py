c82eb0d653837b2baec0b0261231d2ffe1f28674
fa345125-arch <fa345125@gmail.com>
2026-03-12 19:32:14 -0300

    Atualizar o funcionarios.py

--- backend/routers/funcionarios.py
+++ backend/routers/funcionarios.py
@@ -1,7 +1,25 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter()
 
+# modelo de dados
+class Funcionario(BaseModel):
+    nome: str
+    cargo: str
+    salario: float
+
+
+# listar funcionarios
 @router.get("/funcionarios")
 def listar_funcionarios():
     return {"mensagem": "Lista de funcionários"}
+
+
+# cadastrar funcionario
+@router.post("/funcionarios")
+def criar_funcionario(funcionario: Funcionario):
+    return {
+        "mensagem": "Funcionário criado",
+        "dados": funcionario
+    }
