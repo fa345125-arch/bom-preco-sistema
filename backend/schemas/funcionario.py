c073186dac9b7c61da7970e86e8bc2c8a7fbe824
fa345125-arch <fa345125@gmail.com>
2026-03-12 21:47:42 -0300

    Atualizar o funcionario.py

--- backend/schemas/funcionario.py
+++ backend/schemas/funcionario.py
@@ -1,7 +1,27 @@
 from pydantic import BaseModel
+from datetime import date
 
 
 class FuncionarioCreate(BaseModel):
+
     nome: str
+    cpf: str
+    email: str
+    telefone: str
+
     cargo: str
+    departamento: str
+
     salario: float
+    carga_horaria: int
+
+    data_admissao: date
+
+
+class FuncionarioResponse(FuncionarioCreate):
+
+    id: int
+    status: str
+
+    class Config:
+        orm_mode = True
